61fc194c657bc9256bafb436373b814496c3ec0d
Aung Myint Myat <amm926616@gmail.com>
2025-05-09 17:24:48 +0630

    addded colors

---
+++ april-music-player/_utils/test.py
@@ -0,0 +1,11 @@
+from colors import printGreen, printOrange, printRed, printCyan, printYellow
+
+printGreen("This is green color")
+
+printOrange("This is ornage color")
+
+printRed("This is red color")
+
+printCyan("This is cyan color")
+
+printYellow("This is yellow color")
